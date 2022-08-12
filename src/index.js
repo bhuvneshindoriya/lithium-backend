6bc046772a19780dd81c14e9071ba8aa1824594a
Pritesh Kumar <pritesh@gmail.com>
2022-08-12 14:17:56 +0530

    connected to mongodb

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+, {
+   useNewUrlParser: true 
+}
+).then( () => {console.log( "MongoDb is connected")}  )
+.catch( err => console.log(err))
+
+
+
 
 app.use('/', route);
 
