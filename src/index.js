3c465ab839df083c09156e59ca2df263ca0ce964
bhuvneshindoriya <bhuvneshindoriya0909@gmail.com>
2022-10-21 18:44:23 +0530

    done

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://bhuvnesh:bhanu0808@bhuvneshmongo.iskktwc.mongodb.net/bhuvneshmongo"
 , {
    useNewUrlParser: true 
 }
