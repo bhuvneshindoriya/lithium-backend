1e3ec6d7cdbdaa68f1a24d4399d35dbf727a7b3b
Sheetalpandey12 <sheetalpandey555@gmail.com>
2022-11-14 15:45:09 +0530

    blog model created

--- src/Models/BlogModel.js
+++ src/Models/BlogModel.js
@@ -1,10 +1,52 @@
 const mongoose= require ("mongoose");
-
+const ObjectId = mongoose.Schema.Types.ObjectId
 const blogschema = new mongoose.Schema(
 {
+    title: {
+        type : string,
+        require: true,
+
+    },
+     body: {
+        type : string,
+        require: true,
+
+    },
+     authorId: {
+        type :ObjectId,
+        ref:author,
+         require: true,
+
+    },
+     tags: {
+        type:[string],
+
+     },
+     category: {
+        type :string, 
+        require: true,
+
+    },
+     subcategory: [],
+
+     DeletedAt:Date,
     
+       isDeleted: {
+        type :boolean, 
+        default: false
 
-}
+    },
+     publishedAt: {
+        type:Date,
+        default:Date.now,
+     }, 
+
+     isPublished: {
+        type:boolean,
+        default: false
+    },
+  },  { timestamps:true});
+    module.exports = mongoose.models('Blog',authorSchema);  //blogs
 
 
-)
+
