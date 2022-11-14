a6fe96e58225e0a021d4c377bd553e2532f32e88
bhuvneshindoriya <bhuvneshindoriya0909@gmail.com>
2022-11-14 22:34:16 +0530

    done

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,13 +1,19 @@
 const authorModel=require("../Models/AuthorModel")
 const {isValidObjectId} = require ('mongoose')
 const blogModel=require("../Models/BlogModel")
-
+const emailvalidator = require('email-validator')
 
 const createAuthor= async function(req,res){
 try{    const data=req.body;  
-     const savedata=await authorModel.create(data)
- 
-   res.status(201).send({status:true, data:savedata})  
+    let {fname,lname,title,email,password}=data
+    if(!fname && !lname && !title && !password && !email) return res.status(404).send({status:false,message:"All is mandotary"}) 
+
+    let validemail = emailvalidator.validate(email)
+    if(!validemail) return res.status(400).send({status:false,msg:"Invalid E-MAIlID"})
+    let authorValidEmail= await authorModel.findOne({email:email})
+    if(authorValidEmail) return res.status(404).send({status : true,msg:"This email is already registered"})
+    let authorcreate = await authorModel.create(data)
+    res.status(201).send({status:true,data:authorcreate})  
 }
 catch(error){
     res.status(500).send({status:false,message:error.message})
