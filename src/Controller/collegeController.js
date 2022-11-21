f46a7352307379e969b73b30eeb94562ceb6535b
Farzan Khan <kfarzan13@gmail.com>
2022-11-21 15:55:27 +0530

    getCollegeData api done

--- src/Controller/collegeController.js
+++ src/Controller/collegeController.js
@@ -27,25 +27,27 @@ const getCollegeData = async function(req , res){
 
         let filter = req.query
         let { collegeName } = filter
-        console.log(collegeName)
 
         if(Object.keys(filter).length == 0) return res.status(400).send({ status: false, message: "filters are required !!!" })
 
+        if(!collegeName) return res.status(400).send({ status: false, message: "collegeName is required !!!" })
+
         let getCollegeData = await collegeModel.findOne({ name : collegeName }).select({ name : 1 , fullName : 1 , logoLink : 1 })
-        console.log(getCollegeData)
 
         if(!getCollegeData)  return res.status(404).send({ status: false, message: "College not found." })
 
         let collegeId = getCollegeData._id
-        console.log(collegeId)
 
         let dataOfInterns = await internModel.find({collegeId : collegeId}).select({ name : 1 , email : 1 , mobile : 1 })
-        console.log(dataOfInterns)
 
-        getCollegeData.interns = dataOfInterns
-        console.log(getCollegeData)
+        let obj = {
+            name : getCollegeData.name,
+            fullName : getCollegeData.fullName,
+            logoLink : getCollegeData.logoLink,
+            interns : dataOfInterns
+        }
 
-        res.status(200).send({ status: true, data: getCollegeData })
+        res.status(200).send({ status: true, data: obj })
 
     } catch (err) {
         res.status(500).send({ status: false, message: err.message })
