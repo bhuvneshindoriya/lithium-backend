7d55a3923967cfeec4f35ff60593b3ce81ffb0f9
Farzan Khan <kfarzan13@gmail.com>
2022-11-22 16:29:50 +0530

    .

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ app.use(function (req, res) {
     res.status(404).send({ status: false, msg: "Url not found !!!" })
 })
 
-
-app.listen(3000 , function(){
-    console.log("Express app is running on port 3000")
+app.listen(process.env.PORT || 3000 , function(){
+    console.log("Express app is running on port " + (process.env.PORT || 3000))
 })
